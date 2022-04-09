ec5215363be2521aa285c6aa7941a6d0f7571b48
Raven Dorado <rvndorado@gmail.com>
2022-04-09 16:10:44 +0800

    feat(fe): added login page

---
+++ pages/login.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Button, Input } from "semantic-ui-react";
+import Swal from "sweetalert2";
+import { LoginWrapper, LoginForm } from "../presentationals/login";
+import { useAuth } from "../context/AuthContext";
+
+const Login: NextPage = () => {
+  const { login } = useAuth();
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleLogin = (event: React.SyntheticEvent<EventTarget>) => {
+    event.preventDefault();
+    setLoading(true);
+    //@ts-ignore
+    const email = event.target.email.value;
+    //@ts-ignore
+    const password = event.target.password.value;
+    setTimeout(async () => {
+      try {
+        await login(email, password);
+        router.push("/");
+      } catch (error) {
+        Swal.fire(
+          "Invalid email / password",
+          "Please check your email/password entered.",
+          "error"
+        );
+      }
+      setLoading(false);
+    }, 1000);
+  };
+  return (
+    <>
+      <LoginWrapper>
+        <h2>Test Project Login</h2>
+        <LoginForm autoComplete="off" onSubmit={handleLogin}>
+          <Input placeholder="Email" type="email" name="email" required />
+          <Input
+            placeholder="Password"
+            type="password"
+            name="password"
+            required
+          />
+          <Button primary loading={loading} type="submit" disabled={loading}>
+            Login
+          </Button>
+        </LoginForm>
+        <span>
+          No account yet? Click <Link href="/register">here</Link> to register
+        </span>
+      </LoginWrapper>
+    </>
+  );
+};
+
+export default Login;
---
+++ presentationals/login.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+`
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  row-gap: 12px;
+  margin-bottom: 10px;
+`
