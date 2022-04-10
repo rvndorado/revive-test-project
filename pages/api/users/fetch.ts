243507fbaabece4b591da3a97cec585ba28590ec
Raven Dorado <rvndorado@gmail.com>
2022-04-10 16:38:53 +0800

    refactor(api): tweaked fetching from external api

--- pages/api/users/fetch.ts
+++ pages/api/users/fetch.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
-import admin from "../../../firebaseAdmin/firebase"
+import admin from "../../../firebaseAdmin/firebase";
 import { User } from "../../../common/types";
 
 const fetchData = async (
@@ -12,17 +12,29 @@ const fetchData = async (
 
     const usersResponse = await axios.get("https://reqres.in/api/users");
     const users: User[] = usersResponse.data.data;
+    let dataCount = 0;
 
     const batch = firebase.batch();
-    users.forEach(user => {
-      const usersCollection = firebase.collection('users').doc();
+    users.forEach((user) => {
+      const usersCollection = firebase.collection("users").doc();
       batch.set(usersCollection, {
         email: user.email,
         last_name: user.last_name,
         first_name: user.first_name,
         avatar: user.avatar,
-      })
+        date: admin.firestore.Timestamp.now(),
+      });
+      dataCount += 1;
     });
+    batch.set(
+      firebase
+        .collection("user_count")
+        //@ts-ignore
+        .doc(process.env.count_document_id?.toString()),
+      {
+        count: dataCount,
+      }
+    );
     batch.commit();
     return response.status(200).end();
   } catch (error: any) {
